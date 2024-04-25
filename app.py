dcf6d5b2cbe63f5390348883d8c761d2f721e17a
Clay O'Dell <clay.odell.music@gmail.com>
2024-04-25 18:21:50 -0500

    satisfaction survey added

--- app.py
+++ app.py
@@ -3,13 +3,26 @@ from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '110-104-541'
-app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-toolbar = DebugToolbarExtension(app)
 
+responses = []
 
 @app.route("/")
 def root():
     return render_template("root.html")
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/questions/<int:question_id>", methods=['GET', 'POST'])
+def question(question_id):
+    survey = surveys['satisfaction']
+    if question_id >= len(survey.questions):
+        return redirect ("/thank_you")
+    if request.method == 'POST':
+        answer = request.form.get('answer')
+        responses.append(answer)
+        return redirect(f"/questions/{question_id + 1}")
+    else:
+        question = survey.questions[question_id]
+        return render_template('questions.html', question=question)
+    
+@app.route("/thank_you")
+def thank_you():
+    return "Thank you for your answers!"
