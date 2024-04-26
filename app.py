846f5204106f4f3272ded04f7c1809f42838f2db
Clay O'Dell <clay.odell.music@gmail.com>
2024-04-25 19:46:18 -0500

    added code to make sure survey is completed in order

--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template, request, redirect 
+from flask import Flask, render_template, request, redirect, session 
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "110-104-541"
 
-responses = []
 
 @app.route("/")
 def root():
+    session['responses'] = []
     return render_template("root.html")
 
 @app.route("/questions/<int:question_id>", methods=['GET', 'POST'])
 def question(question_id):
     survey = surveys['satisfaction']
-    if question_id >= len(survey.questions):
-        return redirect ("/thank_you")
+    responses = session.get('responses')
+    if responses is None:
+        # trying to access question page too soon
+        return redirect("/")
+    if question_id != len(responses):
+        # trying to access questions out of order
+        return redirect(f"/questions/{len(responses)}")
+    if question_id == len(surveys['satisfaction'].questions):
+        # all questions are answered
+        return redirect("/thank_you")    
     if request.method == 'POST':
         answer = request.form.get('answer')
         responses.append(answer)
+        session['responses'] = responses
         return redirect(f"/questions/{question_id + 1}")
     else:
-        question = survey.questions[question_id]
+        question = surveys['satisfaction'].questions[question_id]
         return render_template('questions.html', question=question)
     
 @app.route("/thank_you")
